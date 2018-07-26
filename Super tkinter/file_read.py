6d29b8775aa0b57a1a71ac22f975f9664b309a35
sun <lijincan1218@qq.com>
2018-07-26 13:30:30 +0800

    增加注释

--- Super tkinter/file_read.py
+++ Super tkinter/file_read.py
@@ -1,3 +1,4 @@
+# 导入模块
 import tkinter
 import os
 import time
@@ -5,21 +6,28 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+
+# 定义类
 class Application():
     def __init__(self):
-
+        # 设置根窗口
         self.root = tkinter.Tk()
-        self.root.title('文件预览')
+        self.root.title('文件预览')  # 设置标题
 
-        self.entryvar = tkinter.StringVar()
-        self.entryvar.set(os.sep)
+        self.entryvar = tkinter.StringVar()  # 获取返回值
+        self.entryvar.set(os.sep)  # 将值初始化
+        # 设置分隔栏
         self.topframe = tkinter.Frame(self.root)
         self.treeframe = tkinter.Frame(self.root)
 
+        # 设置组件
         self.label1 = tkinter.Label(self.topframe, text='当前文件夹:')
         self.entry1 = tkinter.Entry(self.topframe, textvariable=self.entryvar)
         self.button1 = tkinter.Button(self.topframe, text="choose", command=self.catch_file)
-        self.list = ttk.Treeview(self.treeframe, columns=('文件名', '创建日期', '修改日期', 'Size', '是否属于文件夹（TRUE \ FALSE）'), show="headings")
+
+        # 创建文件列表
+        self.list = ttk.Treeview(self.treeframe, columns=('文件名', '创建日期', '修改日期', 'Size', '是否属于文件夹（TRUE \ FALSE）'),
+                                 show="headings")
         # self.image = ImageTk.PhotoImage(Image.open('folder.png'))
         self.list.column('文件名', width=100, anchor='center')
         self.list.column('创建日期', width=100, anchor='center')
@@ -32,12 +40,15 @@ class Application():
         self.list.heading('修改日期', text='修改日期')
         self.list.heading('Size', text='Size')
         self.list.heading('是否属于文件夹（TRUE \ FALSE）', text='是否属于文件夹（TRUE \ FALSE）')
-
+        # 设置事件
         self.tree = ttk.Treeview(self.treeframe)
         self.tree.bind('<<TreeviewSelect>>', self.onTreeviewSelect)
         self.createWidgets()
 
     def createWidgets(self):
+        """
+        绘制
+        """
         self.topframe.pack(fill=tkinter.X)
         self.treeframe.pack(fill=tkinter.X)
         self.label1.grid(row=0, column=0)
@@ -46,10 +57,12 @@ class Application():
         self.tree.pack(fill=tkinter.X)
         self.list.pack(fill=tkinter.X)
 
-
     def catch_file(self):
+        """
+        捕获文件
+        """
         self.tree.delete(*self.tree.get_children())
-        self.start_dir = filedialog.askdirectory()
+        self.start_dir = filedialog.askdirectory()  # 获得起始文件
         self.entryvar.set(self.start_dir)
         self.myid = self.tree.insert('', 0, text=str(self.start_dir))
 
@@ -58,17 +71,21 @@ class Application():
             num = 0
             for each_file in os.listdir(os.curdir):
                 if os.path.isdir(each_file) and each_file[0] != '.':
-                    temp = tree.insert(myid, num, text=str(dir)+os.sep+each_file)
+                    temp = tree.insert(myid, num, text=str(dir) + os.sep + each_file)
                     num += 1
+                    # 递归读取
                     get_dir(dir + os.sep + each_file, tree, temp)
                     os.chdir(os.pardir)
 
         get_dir(self.start_dir, self.tree, self.myid)
-
+        # 更新组件
         self.tree.update()
         self.entry1.update()
 
     def onTreeviewSelect(self, event):
+        """
+        写入列表
+        """
         self.list.delete(*self.list.get_children())
         sels = event.widget.selection()
         for idx in sels:
@@ -79,19 +96,15 @@ class Application():
             os.chdir(filestr)
             for each_file in os.listdir(os.curdir):
                 if each_file[0] != '.':
-                    num+=1
-                    mtime = time.ctime(os.path.getmtime(filestr+os.sep+each_file))
-                    ctime = time.ctime(os.path.getctime(filestr+os.sep+each_file))
-                    size = os.path.getsize(filestr+os.sep+each_file)
-                    isdir = os.path.isdir(filestr+os.sep+each_file)
+                    num += 1
+                    mtime = time.ctime(os.path.getmtime(filestr + os.sep + each_file))
+                    ctime = time.ctime(os.path.getctime(filestr + os.sep + each_file))
+                    size = os.path.getsize(filestr + os.sep + each_file)
+                    isdir = os.path.isdir(filestr + os.sep + each_file)
                     self.list.insert('', num,
-                                     values=(str(each_file), str(ctime), str(mtime), str(size), str(isdir)),
-                                     image=self.image)
-
+                                     values=(str(each_file), str(ctime), str(mtime), str(size), str(isdir)))
 
         self.list.update()
-
-
 
     def onListSelect(self, event):
         sels = event.widget.selection()
